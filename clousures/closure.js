328b38b321ddb59b723118da38c275b71eb6c4ba
Dac099 <davidcejazapata@gmail.com>
2021-04-17 11:50:07 -0500

    Cambio en el archivo closure.js

--- clousures/closure.js
+++ clousures/closure.js
@@ -27,15 +27,56 @@ let myMoney = alcancia();
 myMoney(10); // Imprime 10
 myMoney(15); // Imprime 25
 
-//Ambito lexico
-const f = () => {
-    if(true){
-        var fr = 1;
-        let ft = 2;
-        const fg = 3;
+//Crear variables privadas con closures
+const person = () => {
+    var saveName = "Name";
+    return{
+        getname: () => {
+            return saveName;
+        },
+        setname: (name) => {
+            saveName = name;
+        }
     }
-    console.log(fr);
-    // console.log(ft);
-    // console.log(fg);
 }
-f();
+
+let newPerson = person();
+newPerson.setname("David");
+console.log(newPerson.getname());
+console.log(newPerson.saveName);
+
+/**
+ * Con los closures podemos crear variables privadas a las que no se puede acceder desde fuera de la función, similar a 
+ * getters y setter para las clases.
+ * 
+ * Esto funciona ya que cuando a newPerson se le asigna person() lo que realmente esta pasando es que se invoca la función
+ * por lo que a newPerson se le asigna lo que retorna la función (en este caso es un objeto el que se retorna). Este objeto
+ * tiene las propiedades de de getname y setname para poder manipular la variable saveName, sin embargo si intentamos acceder
+ * a saveName directamente nos saldra como indefinido.
+ */
+
+
+//Tipos de scope
+
+//global scope
+//Declarada en el programa principal. Se puede acceder a ella desde todas las funciones declaradas
+var variable = 'variable global';
+
+//local scope
+const saludo = 'Soy global';
+const saludar = () => {
+    const saludo = 'Soy local';
+    console.log(saludo);
+}
+console.log(saludo);
+saludar();
+
+//Function scope
+const variables = () => {
+    let x = 1;
+    const y = 2;
+    var z = 3;
+}
+console.log(x);
+console.log(y);
+console.log(z);
