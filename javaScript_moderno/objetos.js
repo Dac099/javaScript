44f02137b4ac08ee2cc67683b9d7850ed618c15a
Dac099 <davidcejazapata@gmail.com>
2021-04-20 23:46:23 -0500

    JS moderno

---
+++ javaScript_moderno/objetos.js
@@ -0,0 +1,13 @@
+/**
+ * Una diferencia entre los objetos y los datos primitivos es que los objetos son almacenados y copiados por referencia y
+ * los primitivos son asignados y copiados por valor.
+ */
+
+let phrase = 'Hola';
+let message = 'Adios';
+//Las variables de datos primitivos almacenan el valor que se le haya asignado
+
+let user = {
+    name: 'David',
+    id: 1604
+}
