d80dd4b585c3ffc670c2e54a66eb87c593527b3e
Dac099 <davidcejazapata@gmail.com>
2021-06-11 12:25:35 -0500

    c

--- ajax/src/index.js
+++ ajax/src/index.js
@@ -2,14 +2,20 @@ const URL = "https://jsonplaceholder.typicode.com/users";
 //Se instancia un objeto XMLHttpRequest
 let http = new XMLHttpRequest();
 http.open("GET", URL);
-http.onreadystatechange = function(event) {
-    if(http.readyState == 4){
-        if(http.status == 200){
-            console.log(JSON.parse(http.responseText)[0]);
-        }else{
-            console.log("Error");
-        }
-    }
-};
+
+//La funcion onload tiene el mismo resultado que usar el codigo comentado
+http.onload = () => {
+    const json = JSON.parse(http.responseText);
+    console.log(json);
+}
+// http.onreadystatechange = function(event) {
+//     if(http.readyState == 4){
+//         if(http.status == 200){
+//             console.log(JSON.parse(http.responseText)[0]);
+//         }else{
+//             console.log("Error");
+//         }
+//     }
+// };
 http.send();
 
