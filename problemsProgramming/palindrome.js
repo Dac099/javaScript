ff3895149d4b3080f88081e08252ed5a2d433455
Dac099 <davidcejazapata@gmail.com>
2021-07-12 21:27:55 -0500

    regex

--- problemsProgramming/palindrome.js
+++
@@ -1,7 +0,0 @@
-var isPalindrome = function(x){
-    if(String(x).split("").reverse("").join("") === String(x)){
-        return true;
-    }else{
-        return false;
-    }
-}
--- problemsProgramming/retoCumpleaños.js
+++
@@ -1,5 +0,0 @@
-let myBirthday = new Date(1999, 03, 16);
-let today = new Date();
-const MDAY = 24 * 60 * 60 * 1000 * 24;
-
-let days = Math.trunc((today - myBirthday)/ MDAY);
